a546fb44b7d8fcc81c0afeada4a247d37a825d4a
Carla Brady <carlakaybrady@gmail.com>
2017-10-02 13:04:33 -0500

    changes for mlab

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ var homesRoute = require('./modules/routes/homesRoute.js');
 var mongoose = require('mongoose');
 
 // middleware
-mongoose.connect('mongodb://localhost:27017/realestate');
+// mongoose.connect('mongodb://agent:realestate@ds157444.mlab.com:57444/heroku_fm84mlp0');
+
+var databaseURI = '';
+// process.env.MONGODB_URI will only be defined if you 
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    databaseURI = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    databaseURI = 'mongodb://localhost:27017/realestate';
+}
+
+mongoose.connect(databaseURI);
+
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
